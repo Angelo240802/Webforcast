92ebb77c5d4c601dfcc5d33059411ff267aa8520
Angelo240802 <joseemanuellangelogarcia@gmail.com>
2025-07-14 13:44:10 +0700

    add jga in jose branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,9 @@ function App() {
        <p className="read-the-docs">
         this is jose
       </p>
-        
+       <p className="read-the-docs">
+        add jga in jose branch
+      </p>  
     </>
   )
 }
